0576f875e7495464b349f68e2e53c9511512408b
arkharman12 <53625021+arkharman12@users.noreply.github.com>
2019-08-23 21:51:21 -0400

    created Horse.h

---
+++ base/Horse.h
@@ -0,0 +1,14 @@
+//Horse.h
+#ifndef HORSE_H_EXISTS
+#define HORSE_H_EXISTS
+
+class Horse{
+    private:
+        int position;
+    public:
+        Horse();
+        void advance();
+        int getPosition();
+};
+
+#endif
